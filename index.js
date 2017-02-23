78ffe86c6fbf35ccc43012d192d9a6b13d7f0dc2
Oleg Gaidarenko <markelog@gmail.com>
2017-02-23 19:44:58 +0100

    Make travis pass

---
+++ index.js
@@ -0,0 +1,16 @@
+require('./monkey-patch').addPlugins(['eslint-config-airbnb-base']);
+
+module.exports = {
+  extends: require.resolve('eslint-config-airbnb-base'),
+
+  rules: {
+    'no-param-reassign': ['off'],
+    'arrow-body-style': ['off'],
+    'no-console': ['off'],
+    'class-methods-use-this': ['off'],
+    'import/no-dynamic-require': ['off'],
+    'global-require': ['off'],
+    'comma-dangle': ['off'],
+    'no-useless-return': ['off']
+  }
+};
--- monkey-patch.js
+++ monkey-patch.js
@@ -1,11 +1,11 @@
-var Module = require("module");
-var path = require("path");
+const Module = require('module');
+const path = require('path');
 
-var eslintLoc;
-var parent = module;
-while(parent) {
+let eslintLoc;
+let parent = module;
+while (parent) {
   try {
-    eslintLoc = Module._resolveFilename("eslint", parent);
+    eslintLoc = Module._resolveFilename('eslint', parent);
     break;
   } catch (err) {
     parent = parent.parent;
@@ -15,19 +15,21 @@ if (!eslintLoc) {
   throw new ReferenceError("couldn't resolve eslint");
 }
 
-var pluginPath = path.resolve(eslintLoc, "..", "config", "plugins.js");
-var plugins = require(pluginPath);
-var oldLoad = plugins.load.bind(plugins);
-var myPlugins = [];
-plugins.load = function(pluginName) {
+const pluginPath = path.resolve(eslintLoc, '..', 'config', 'plugins.js');
+const plugins = require(pluginPath);
+const oldLoad = plugins.load.bind(plugins);
+let myPlugins = [];
+
+plugins.load = (pluginName) => {
   if (myPlugins.indexOf(pluginName) === -1) {
     return oldLoad(pluginName);
   }
-  this.define(pluginName, require("eslint-plugin-" + pluginName));
-}
+
+  this.define(pluginName, require(`eslint-plugin-${pluginName}`));
+};
 
 module.exports = {
-  addPlugins: function(plugins) {
-    myPlugins = myPlugins.concat(plugins);
+  addPlugins(additions) {
+    myPlugins = myPlugins.concat(additions);
   }
-}
+};
